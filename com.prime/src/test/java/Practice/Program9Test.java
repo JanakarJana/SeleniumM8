8de15176f7959a680142ab6780cbccba62159791
JanakarJana <janakarjana1408@gmail.com>
2025-03-09 12:08:50 +0530

    updated

---
+++ com.prime/src/test/java/Practice/Program9Test.java
@@ -0,0 +1,14 @@
+package Practice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import GenericLibrary.BaseClass;
+
+public class Program9Test extends BaseClass {
+
+	@Test
+	public void testCase() {
+		Reporter.log("hi");
+	}
+}
